6548dcf9efe09d176581fcef4b9cf4fa4a3b4bbb
smartjoban <74201550+smartjoban@users.noreply.github.com>
2021-12-26 17:00:51 +0000

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,17 @@
+def swap():
+    file1=input("Enter the file1 name:")
+    file2=input("Enter the file2 name:")
+
+    with open(file1,'r') as a:
+        data_a=a.read()
+
+    with open(file2,'r') as b:
+        data_b=b.read()
+          
+    with open(file1,'w') as a:
+        a.write(data_b)   
+
+    with open(file2,'w') as b:
+        b.write(data_a)      
+
+swap()    
